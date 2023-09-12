Fixed input Image normalization. Works for BS=1 only.

demo_ov.py
@@ -2,17 +2,13 @@ import string
 import argparse
 
 import torch
-import torch.backends.cudnn as cudnn
 import torch.utils.data
 import torch.nn.functional as F
 
 from utils import CTCLabelConverter, AttnLabelConverter
 from dataset import RawDataset, AlignCollate
-from model import Model
 
-from openvino.runtime import Core
-from openvino.preprocess import PrePostProcessor
-from openvino.runtime import Layout, Type
+from openvino.runtime import Core, get_version
 
 def demo(opt):
     """ model configuration """
@@ -25,37 +21,27 @@ def demo(opt):
     if opt.rgb:
         opt.input_channel = 3
 
-    print('model input parameters', opt.imgH, opt.imgW, opt.num_fiducial, opt.input_channel, opt.output_channel,
-          opt.hidden_size, opt.num_class, opt.batch_max_length, opt.Transformation, opt.FeatureExtraction,
-          opt.SequenceModeling, opt.Prediction)
+    print('Model input parameters:')
+    opt_dict = vars(opt)
+    for key, value in opt_dict.items():
+        print(f"{key}: {value}")
 
+    print(f"\nOpenVINO Version: {get_version()}")
 
     # load model
-    print('loading pretrained model from %s' % opt.saved_model)
-    model_xml = "models-exported/TPS-ResNet-BiLSTM-Attn_fp32.xml"
+    print(f"Loading pretrained model from {opt.saved_model}")
+    model_xml = opt.saved_model
 
     core = Core()
-    core.set_property("CPU", {"INFERENCE_PRECISION_HINT": "f32"})
     model = core.read_model(model=model_xml)
-    model.reshape({"input.1":[opt.batch_size,1,32,100]})
-    # model.reshape({"input.1":[opt.batch_size,1,32,100],"onnx::Gather_1":[opt.batch_size,26]})
-    # model.reshape({"1":[opt.batch_size,1,32,100],"2":[opt.batch_size,26]})
+    input_layer = model.input(0)
+    model.reshape({input_layer:[opt.batch_size,opt.input_channel,opt.imgH,opt.imgW]}) #{"onnx::Sub_0":[1,1,32,100]}
 
-    ppp = PrePostProcessor(model)
-    ppp.input(0).tensor().set_element_type(Type.f32).set_layout(Layout('NCHW'))
-    # ppp.input(1).tensor().set_element_type(Type.i64)
+    compiled_model = core.compile_model(model=model, device_name=opt.device.upper())
 
-    #print(f'Dump preprocessor: {ppp}')
-    model = ppp.build()
-
-    inference_precision = core.get_property("CPU", "INFERENCE_PRECISION_HINT")
-    print(f"Inference Precision: {inference_precision}")
-
-    compiled_model = core.compile_model(model=model, device_name="CPU")
-
-    input_layer = compiled_model.input(0)
-    print(f"Input Layer: {input_layer}")
     output_layer = compiled_model.output(0)
+    print(f"Input Layer: {input_layer}")
+    print(f"Output Layer: {output_layer}")
 
     # prepare data. two demo images from https://github.com/bgshih/crnn#run-demo
     AlignCollate_demo = AlignCollate(imgH=opt.imgH, imgW=opt.imgW, keep_ratio_with_pad=opt.PAD)
@@ -74,20 +60,14 @@ def demo(opt):
         length_for_pred = torch.IntTensor([opt.batch_max_length] * batch_size).to(opt.device)
         text_for_pred = torch.LongTensor(batch_size, opt.batch_max_length + 1).fill_(0).to(opt.device)
 
-        # print(f"length_for_pred shape: {length_for_pred.shape}")
-        # print(f"text_for_pred shape: {length_for_pred.shape}")
-
         if 'CTC' in opt.Prediction:
             preds = compiled_model(image, text_for_pred)[output_layer]
-
             # Select max probabilty (greedy decoding) then decode index to character
             preds_size = torch.IntTensor([preds.size(1)] * batch_size)
             _, preds_index = preds.max(2)
             # preds_index = preds_index.view(-1)
             preds_str = converter.decode(preds_index, preds_size)
-
         else:
-
             # preds = compiled_model((image, text_for_pred))[output_layer]
             preds = compiled_model(image)[output_layer]
             # print(f"preds shape: {preds.shape}")
@@ -122,12 +102,13 @@ def demo(opt):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--eval_data', default='evaluation/', required=False, help='path to evaluation dataset')
-    parser.add_argument('--data_filtering_off', action='store_true', help='for data_filtering_off mode')
-    parser.add_argument('--image_folder', default='demo_image/', required=False, help='path to image_folder which contains text images')
-    parser.add_argument('--workers', type=int, help='number of data loading workers', default=4)
-    parser.add_argument('--batch_size', type=int, default=192, help='input batch size')
     parser.add_argument('--saved_model', default='models-exported/TPS-ResNet-BiLSTM-Attn_fp32.xml', required=False, help="path to OV XML to evaluation")
+    parser.add_argument('--image_folder', default='demo_image/', required=False, help='path to image_folder which contains text images')
+    parser.add_argument('--eval_data', default='evaluation/', required=False, help='path to evaluation dataset')
+    parser.add_argument('--device', default='cpu', required=False, help='Target device to run on')
+    parser.add_argument('--data_filtering_off', action='store_true', help='for data_filtering_off mode')
+    parser.add_argument('--workers', type=int, help='number of data loading workers', default=4)
+    parser.add_argument('--batch_size', type=int, default=1, help='input batch size')
     """ Data processing """
     parser.add_argument('--batch_max_length', type=int, default=25, help='maximum-label-length')
     parser.add_argument('--imgH', type=int, default=32, help='the height of the input image')
@@ -152,12 +133,5 @@ if __name__ == '__main__':
     """ vocab / character number configuration """
     if opt.sensitive:
         opt.character = string.printable[:-6]  # same with ASTER setting (use 94 char).
-
-    opt.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    print(f"Device: {opt.device}")
-
-    cudnn.benchmark = True
-    cudnn.deterministic = True
-    opt.num_gpu = torch.cuda.device_count()
 
     demo(opt)

export_to_ov_ir.py
@@ -1,3 +1,12 @@
+"""
+Usage: 
+To export models for OVMS:
+python export_to_ov_ir.py --output_dir models-exported-ovms --ovms
+
+To export models to use with demo_ov.py
+python export_to_ov_ir.py
+"""
+
 import string
 import argparse
 from pathlib import Path
@@ -7,6 +16,15 @@ from model import Model
 import torch
 from openvino.runtime import serialize
 from openvino.tools import mo
+
+# Wrapper for Model to include input normalization
+class EncDecWrapper(Model):
+    def __init__(self, opt):
+        super(EncDecWrapper, self).__init__(opt)
+
+    def forward(self, input, text,is_train=False):
+        input.sub_(127.5).div_(127.5)  # Normalize input image.
+        return super().forward(input, text, is_train)
 
 def export_to_ov_ir(opt):
     """ model configuration """
@@ -19,7 +37,10 @@ def export_to_ov_ir(opt):
     if opt.rgb:
         opt.input_channel = 3
 
-    model = Model(opt)
+    if  opt.ovms:
+        model = EncDecWrapper(opt)
+    else:
+        model = Model(opt)
 
     print('Model input parameters', opt.imgH, opt.imgW, opt.num_fiducial, opt.input_channel, opt.output_channel,
           opt.hidden_size, opt.num_class, opt.batch_max_length, opt.Transformation, opt.FeatureExtraction,
@@ -27,12 +48,13 @@ def export_to_ov_ir(opt):
 
     model = torch.nn.DataParallel(model).to(opt.device)
 
-    # load model
-    print('loading pretrained model from %s' % opt.saved_model)
+   # load model
+    print(f"Loading pretrained model from {opt.saved_model}")
+
     model.load_state_dict(torch.load(opt.saved_model, map_location=opt.device))
     model = model.module # Unwrap the model from DataParallel using the .module attribute
 
-    # predict
+    # Set model to Eval
     model.eval()
 
     # Create dummy inputs
@@ -42,15 +64,23 @@ def export_to_ov_ir(opt):
 
     # Save FP32 ONNX and IR model
     torch.onnx.export(model, (dummy_input, dummy_input_txt), opt.fp32_onnx_path, opset_version=16)
+    # Export to IR
     model_ir = mo.convert_model(input_model=opt.fp32_onnx_path)
     serialize(model_ir, str(opt.fp32_ir_path))
 
-    # from openvino.tools.mo import convert_model
-    # ov_model = convert_model(model, example_input=(dummy_input, dummy_input_txt) )
-    # serialize(ov_model, str(opt.fp32_ir_path))
+    # Export with Dynamic Batch size.
+    # torch.onnx.export(model,
+    #               (dummy_input, dummy_input_txt),
+    #               opt.fp32_onnx_path,
+    #               dynamic_axes={"input.1": {0: "-1", 1:"1", 2:"32", 3:"100"}, "onnx::Gather_1": {0: "-1", 1:"26"}},
+    #               input_names=["input.1","onnx::Gather_1"],
+    #               opset_version=16)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+    parser.add_argument('--ovms', action='store_true', default=False, help='Export for OVMS. Set False to use with demo_ov.py')
+    parser.add_argument('--output_dir', default="models-exported", required=False, help='Output Dir for exported models.')
     parser.add_argument('--saved_model', default='models/TPS-ResNet-BiLSTM-Attn.pth', required=False, help="path to saved_model")
     """ Data processing """
     parser.add_argument('--batch_max_length', type=int, default=25, help='maximum-label-length')
@@ -77,10 +107,10 @@ if __name__ == '__main__':
     if opt.sensitive:
         opt.character = string.printable[:-6]  # same with ASTER setting (use 94 char).
 
-    opt.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    opt.device = 'cpu'
     print(f"Device: {opt.device}")
 
-    output_dir = Path("models-exported")  # Update OUTPUT_DIR to opt.output_dir
+    output_dir = Path(opt.output_dir)  # Update OUTPUT_DIR to opt.output_dir
     base_model_name = opt.saved_model.split('/')[-1].split('.')[0] # Get the filename only, ex: TPS-ResNet-BiLSTM-Attn
     output_dir.mkdir(exist_ok=True)
 
@@ -91,5 +121,10 @@ if __name__ == '__main__':
     export_to_ov_ir(opt)
 
     print()
-    print(f"ONNX model saved at: {opt.fp32_onnx_path}")
-    print(f"OpenVINO IR model saved at: {opt.fp32_ir_path}")
+    if  opt.ovms:
+        print(f"ovms flag is set to True. So, the model is exported for OVMS usage. Model includes image normalization")
+    else:
+        print(f"ovms flag is set to False. So, the model is exported for API usage (demo_ov.py). Input image is normalized via AlignCollate function.")
+
+    print(f"Use in OVMS: {opt.ovms}. ONNX model saved at: {opt.fp32_onnx_path}")
+    print(f"Use in OVMS: {opt.ovms}. OpenVINO IR model saved at: {opt.fp32_ir_path}")